6e941f8e3a6cbb92d2635213c8ef21de5735004e
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-20 13:02:23 -0300

    iniciando projeto, criando classe principal e classe de requisicao de consumo de api

---
+++ src/main/java/br/com/eder/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,25 @@
+package br.com.eder.screenmatch2;
+
+import br.com.eder.screenmatch2.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&Season=1&apikey=2389347b");
+		System.out.println(json);
+		//"https://coffee.alexflipnote.dev/random.json" imagem de cafe
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
