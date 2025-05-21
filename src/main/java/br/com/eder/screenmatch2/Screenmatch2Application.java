4a87f5416223eed1a64c1230b802cabeb3e7910c
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-21 09:22:07 -0300

    criando uma interface com metodo generico para conversao de dados

--- src/main/java/br/com/eder/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/eder/screenmatch2/Screenmatch2Application.java
@@ -1,6 +1,8 @@
 package br.com.eder.screenmatch2;
 
+import br.com.eder.screenmatch2.model.DadosSerie;
 import br.com.eder.screenmatch2.service.ConsumoApi;
+import br.com.eder.screenmatch2.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +18,15 @@ public class Screenmatch2Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&Season=1&apikey=2389347b");
-		System.out.println(json);
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&apikey=2389347b");
+		//System.out.println(json);
 		//"https://coffee.alexflipnote.dev/random.json" imagem de cafe
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
+		//json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
 
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
+		System.out.println(dados);
 
 	}
 }
--- src/main/java/br/com/eder/screenmatch2/service/ConverteDados.java
+++ src/main/java/br/com/eder/screenmatch2/service/ConverteDados.java
@@ -1,4 +1,19 @@
 package br.com.eder.screenmatch2.service;
 
-public class ConverteDados {
+import br.com.eder.screenmatch2.model.DadosSerie;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
