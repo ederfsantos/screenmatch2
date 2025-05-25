888c8363a05cc343708bec80b827713f27adab7e
Eder Ferreira Santos <ederfsantos@live.com>
2025-05-25 13:25:21 -0300

    crinado classe principal e aprendendo expressao lambda

--- src/main/java/br/com/eder/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/eder/screenmatch2/Screenmatch2Application.java
@@ -3,6 +3,7 @@ package br.com.eder.screenmatch2;
 import br.com.eder.screenmatch2.model.DadosEpisodio;
 import br.com.eder.screenmatch2.model.DadosSerie;
 import br.com.eder.screenmatch2.model.DadosTemporada;
+import br.com.eder.screenmatch2.principal.Principal;
 import br.com.eder.screenmatch2.service.ConsumoApi;
 import br.com.eder.screenmatch2.service.ConverteDados;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,48 +27,40 @@ public class Screenmatch2Application implements CommandLineRunner {
         SpringApplication.run(Screenmatch2Application.class, args);
     }
 
+    //"https://coffee.alexflipnote.dev/random.json" imagem de cafe
+//        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+//        System.out.println(json);
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&apikey=2389347b");
-        //System.out.println(json);
-        //"https://coffee.alexflipnote.dev/random.json" imagem de cafe
-        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
 
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dadosSerie);
-        json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&Season=1&episode=1&apikey=2389347b");
-        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-        System.out.println(dadosEpisodio);
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-        DadosTemporada dadosTemporada;
-
-        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-            json = consumoApi.obterDados("https://www.omdbapi.com/?t=er&Season=" + i + "&apikey=2389347b");
-            dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-
-        }
-        //exibindo a lista de temporadas
-        temporadas.forEach(System.out::println);
-        // Gravando em arquivo de texto no formato json o resultado
-        //usando Jackson
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);//para vir indentado o json
-        mapper.writeValue(new File("saidaMapperJackson.json"), temporadas);
-        //System.out.println();
-
-        //usando Gson
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();//para vir indentado o json
-       // String resposta = gson.toJson(temporadas);
-       // System.out.println(resposta);
-        FileWriter file = new FileWriter("saidaComGson.json");
-        file.write(gson.toJson(temporadas));
+        Principal principal = new Principal();
+        principal.exibeMenu();
 
 
 
+//        List<DadosTemporada> temporadas = new ArrayList<>();
+//        DadosTemporada dadosTemporada;
+////https://www.omdbapi.com/?t=grey's+anatomy&Season=1&apikey=2389347b
+//        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+//            json = consumoApi.obterDados("https://www.omdbapi.com/?t=vikings&Season=" + i + "&apikey=2389347b");
+//            dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//            temporadas.add(dadosTemporada);
+//
+//        }
+//        //exibindo a lista de temporadas
+//        temporadas.forEach(System.out::println);
+//        // Gravando em arquivo de texto no formato json o resultado
+//        //usando Jackson
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.enable(SerializationFeature.INDENT_OUTPUT);//para vir indentado o json
+//        mapper.writeValue(new File("saidaMapperJackson.json"), temporadas);
+//
+//        //usando Gson
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();//para vir indentado o json
+//        // String resposta = gson.toJson(temporadas);
+//        // System.out.println(resposta);
+//        FileWriter file = new FileWriter("saidaComGson.json");
+//        file.write(gson.toJson(temporadas));
+//        file.close();
     }
 }
---
+++ src/main/java/br/com/eder/screenmatch2/principal/Principal.java
@@ -0,0 +1,51 @@
+package br.com.eder.screenmatch2.principal;
+
+import br.com.eder.screenmatch2.model.DadosEpisodio;
+import br.com.eder.screenmatch2.model.DadosSerie;
+import br.com.eder.screenmatch2.model.DadosTemporada;
+import br.com.eder.screenmatch2.service.ConsumoApi;
+import br.com.eder.screenmatch2.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=2389347b";
+
+    public void exibeMenu() {
+        System.out.println("Digie o nome da série para consultar: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+        //ENDERECO + nomeSerie.replace(" ","+")+API_KEY;
+        //"https://www.omdbapi.com/?t=vikings&apikey=2389347b"
+        List<DadosTemporada> temporadas = new ArrayList<>();
+        DadosTemporada dadosTemporada;
+//https://www.omdbapi.com/?t=grey's+anatomy&Season=1&apikey=2389347b
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+        temporadas.forEach(System.out::println);
+
+//        List<DadosEpisodio> episodiosTemporada;
+//        for (int i = 0; i < dadosSerie.totalTemporadas(); i++) {
+//            episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t ->t.episodios().forEach(e-> System.out.println(e.titulo())));
+
+
+    }
+}
